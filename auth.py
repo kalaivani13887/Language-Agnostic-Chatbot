0f86d6ea763eadf8cf68e0377ed015b9cb7ac602
kalaivani13887 <kalaivaniravi47@gmail.com>
2026-06-29 09:53:23 +0530

    README.md

---
+++ auth.py
@@ -0,0 +1,79 @@
+============================================================
+
+auth.py
+
+Handles Registration, Login and User Storage
+
+============================================================
+
+import json
+import os
+import hashlib
+
+USER_DB = "users.json"
+
+def hash_password(password):
+return hashlib.sha256(
+password.encode()
+).hexdigest()
+
+def load_users():
+
+if not os.path.exists(USER_DB):  
+
+    with open(USER_DB, "w") as f:  
+        json.dump({}, f)  
+
+with open(USER_DB, "r") as f:  
+    return json.load(f)
+
+def save_users(users):
+
+with open(USER_DB, "w") as f:  
+    json.dump(users, f, indent=2)
+
+def register_user(
+full_name,
+username,
+password
+):
+
+users = load_users()  
+
+if username in users:  
+    return False  
+
+users[username] = {  
+
+    "full_name": full_name,  
+
+    "password":  
+    hash_password(password)  
+
+}  
+
+save_users(users)  
+
+return True
+
+def login_user(
+username,
+password
+):
+
+users = load_users()  
+
+if username not in users:  
+    return None  
+
+if (  
+    users[username]["password"]  
+    ==  
+    hash_password(password)  
+):  
+
+    return users[username][  
+        "full_name"  
+    ]  
+
+return None language agnostic chatbot project correct thana
